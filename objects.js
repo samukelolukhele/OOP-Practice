6f37b2ba02335353c91c833e6738209e9eb8b39d
Samukel Lukhele <kelo.lukhele@gmail.com>
2022-03-15 18:42:19 +0200

    Learned more about method chaining and this

--- objects.js
+++ objects.js
@@ -1,13 +1,40 @@
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
-    }
-};
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+    this.score = 0;
+  }
+  login() {
+    console.log(this.email, "just logged in");
+    return this;
+  }
 
-userOne.login();
-userOne.logout();
+  logout() {
+    console.log(this.email, "just logged out");
+    return this;
+  }
+  updateScore() {
+    this.score++;
+    console.log(this.email, "score is now", this.score);
+    return this;
+  }
+}
+
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter(u => {
+        console.log(u)
+      return u.email != user.email;
+    });
+  }
+}
+
+var userOne = new User("ryu@ninjas.com", "Ryu");
+var userTwo = new User("yoshi@mariokorp.com", "Yoshi");
+var admin = new Admin("ryzo@ninjas.com", "Ryzo");
+
+admin.deleteUser(userTwo);
+
+var users = [userOne, userTwo];
+
+console.log(users);
