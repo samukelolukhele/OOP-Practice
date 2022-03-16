d3ed7210dafe11e35653807faa5968890865bea6
Samukel Lukhele <kelo.lukhele@gmail.com>
2022-03-16 07:14:26 +0200

    Learned about constructor functions, prototypes and prototype inheritance

--- objects.js
+++ objects.js
@@ -1,40 +1,36 @@
-class User {
-  constructor(email, name) {
+function User(email, name){
     this.email = email;
     this.name = name;
-    this.score = 0;
-  }
-  login() {
-    console.log(this.email, "just logged in");
-    return this;
-  }
-
-  logout() {
-    console.log(this.email, "just logged out");
-    return this;
-  }
-  updateScore() {
-    this.score++;
-    console.log(this.email, "score is now", this.score);
-    return this;
-  }
+    this.online = false;
 }
 
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter((u) => {
-      return u.email != user.email;
-    });
-  }
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, 'has logged in');
 }
+
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.email, 'has logged out');
+}
+
+function Admin(...args){
+    User.apply(this, args);
+    this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.email != u.email;
+    })
+};
 
 var userOne = new User("ryu@ninjas.com", "Ryu");
 var userTwo = new User("yoshi@mariokorp.com", "Yoshi");
-var admin = new Admin("ryzo@ninjas.com", "Ryzo");
+var admin = new Admin('taicho@ninjas.com', 'Taicho');
 
 var users = [userOne, userTwo, admin];
 
-admin.deleteUser(userOne);
-
-
-console.log(users);
+console.log(admin);
